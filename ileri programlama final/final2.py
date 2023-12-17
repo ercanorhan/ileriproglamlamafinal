c4c7de32e8b86666efca7ab6fd40982515b02d93
Ercan ORHAN <88620348+ercanorhan@users.noreply.github.com>
2023-12-17 23:56:01 +0300

    Add files via upload

--- ileri programlama final/final2.py
+++ ileri programlama final/final2.py
@@ -9,12 +9,13 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 import warnings
 
 warnings.filterwarnings("ignore")
 data=pd.read_csv('california_housing.csv')
 data.dropna(inplace=True)
+
 print(data.info())
 data = data.join(pd.get_dummies(data.ocean_proximity)).drop(['ocean_proximity'],axis=1)
 
@@ -25,55 +26,91 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42
 train_data=x_train.join(y_train)
 
 
-# train_data_wo_proximity=train_data.drop(['ocean_proximity'],axis=1)
-# train_data_wo_proximity.corr()
 plt.figure(figsize=(15,8))
 sns.heatmap(train_data.corr(),annot=True,cmap='RdYlGn')
 
-train_data['total_rooms']=np.log(train_data['total_rooms'])
-train_data['total_bedrooms']=np.log(train_data['total_bedrooms'])
-train_data['population']=np.log(train_data['population'])
-train_data['households']=np.log(train_data['households'])
+train_data['total_rooms']=np.log(train_data['total_rooms']+1)
+train_data['total_bedrooms']=np.log(train_data['total_bedrooms']+1)
+train_data['population']=np.log(train_data['population']+1)
+train_data['households']=np.log(train_data['households']+1)
+train_data['bedroomratio']=train_data['total_bedrooms']/train_data['total_rooms']
+train_data['householdrooms']=train_data['total_rooms']/train_data['households']
+train_data.hist(figsize=(15,8))
 
-# train_data.ocean_proximity.value_counts()
-# train_data = train_data.join(pd.get_dummies(train_data.ocean_proximity)).drop(['ocean_proximity'],axis=1)
 plt.figure(figsize=(15,8))
-sns.heatmap(train_data.corr(),annot=True,cmap='YlGnBu')
 sns.scatterplot(x='latitude',y='longitude',data=train_data,hue='median_house_value',palette="coolwarm")
 
-# train_data['bedroom_ratio']=train_data['total_bedrooms']/train_data['total_rooms']
-# train_data['household_rooms']=train_data['total_rooms']/train_data['households']
-
-# x_train,y_train=train_data.drop(['median_house_value'],axis=1),train_data['median_house_value']
-# scaler=StandardScaler()
-# x_train_s=scaler.fit_transform(x_train)
 
 test_data=x_test.join(y_test)
-test_data['total_rooms']=np.log(test_data['total_rooms'])
-test_data['total_bedrooms']=np.log(test_data['total_bedrooms'])
-test_data['population']=np.log(test_data['population'])
-test_data['households']=np.log(test_data['households'])
+test_data['total_rooms']=np.log(test_data['total_rooms']+1)
+test_data['total_bedrooms']=np.log(test_data['total_bedrooms']+1)
+test_data['population']=np.log(test_data['population']+1)
+test_data['households']=np.log(test_data['households']+1)
+test_data['bedroomratio']=test_data['total_bedrooms']/test_data['total_rooms']
+test_data['householdrooms']=test_data['total_rooms']/test_data['households']    
 
-# reg=LinearRegression()
-# reg.fit(x_train,y_train)
-# reg.score(x_test,y_test)
+print('###############################################################')
+print('linear regression metodu')
+reg=LinearRegression()
+reg.fit(x_train,y_train)
+reg_score=reg.score(x_test,y_test)
+y_pred = reg.predict(x_test)
+y_comp=pd.DataFrame(y_test)
+y_comp2=y_comp.join(pd.DataFrame({'prediction':y_pred}).set_index(y_test.index))
+print(y_comp2.head(25))
+print('linear regression skoru:'+ f'{reg.score(x_test,y_test)}')    
 
-
+print('###############################################################')
+print('random forest metodu')
 forest=RandomForestRegressor()
 forest.fit(x_train,y_train)
-forest.score(x_test,y_test)
+forest_score=forest.score(x_test,y_test)
 y_pred = forest.predict(x_test)
 y_comp=pd.DataFrame(y_test)
 y_comp1=y_comp.join(pd.DataFrame({'prediction':y_pred}).set_index(y_test.index))
-print(y_comp1)
+print(y_comp1.head(25))
+print('random forest skoru:'+ f'{forest.score(x_test,y_test)}')
 
-# knn=KNeighborsRegressor()
-# knn.fit(x_train,y_train)
-# knn.score(x_test,y_test)
+print('###############################################################')
+print('svr metodu')
+svr=SVR()
+svr.fit(x_train,y_train)
+svr_score=svr.score(x_test,y_test)
+y_pred = svr.predict(x_test)
+y_comp=pd.DataFrame(y_test)
+y_comp3=y_comp.join(pd.DataFrame({'prediction':y_pred}).set_index(y_test.index))
+print(y_comp3.head(25))
+print('svr skoru:'+ f'{svr.score(x_test,y_test)}')
 
-# mlp=MLPRegressor()
-# mlp.fit(x_train,y_train)
-# mlp.score(x_test,y_test)
+print('###############################################################')
+print('knn metodu')
+knn=KNeighborsRegressor()
+knn.fit(x_train,y_train)
+knn_score=knn.score(x_test,y_test)
+y_pred = knn.predict(x_test)
+y_comp=pd.DataFrame(y_test)
+y_comp4=y_comp.join(pd.DataFrame({'prediction':y_pred}).set_index(y_test.index))
+print(y_comp4.head(25))
+print('knn skoru:'+ f'{knn.score(x_test,y_test)}')
+
+print('###############################################################')
+print('mlp metodu')
+mlp=MLPRegressor()
+mlp.fit(x_train,y_train)
+mlp_score=mlp.score(x_test,y_test)
+y_pred = mlp.predict(x_test)
+y_comp=pd.DataFrame(y_test)
+y_comp5=y_comp.join(pd.DataFrame({'prediction':y_pred}).set_index(y_test.index))
+print(y_comp5.head(25))
+print('mlp skoru:'+ f'{mlp.score(x_test,y_test)}') 
+
+
+print('###############################################################')
+
+y=[reg_score,forest_score,svr_score,knn_score,mlp_score]
+x=['linear regression','random forest','svr','knn','mlp']
+plt.figure(figsize=(15,8))
+sns.barplot(x=x,y=y).set(title='Metodlar')
 
 # param_grid = { 
 #     "n_estimators": [200, 500],
